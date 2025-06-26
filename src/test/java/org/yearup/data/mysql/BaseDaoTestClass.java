53b5cb682d48e042f313e1aea291a0904fce450f
Pinar Durmaz <pnr.drmz02@gmail.com>
2025-06-26 00:24:46 -0400

    Complated Capstone-3 Project EasyShop Application

---
+++ src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
@@ -0,0 +1,34 @@
+package org.yearup.data.mysql;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ActiveProfiles; // added
+import org.springframework.test.context.TestPropertySource; // added
+import org.yearup.configuration.TestDatabaseConfig;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestDatabaseConfig.class)
+@ActiveProfiles("test") // added
+@TestPropertySource("classpath:application-test.properties") // added
+public abstract class BaseDaoTestClass
+{
+    @Autowired
+    protected DataSource dataSource;
+
+    @AfterEach
+    public void rollback() throws SQLException
+    {
+        Connection connection = dataSource.getConnection();
+        if(!connection.getAutoCommit())
+        {
+            dataSource.getConnection().rollback();
+        }
+    }
+}
